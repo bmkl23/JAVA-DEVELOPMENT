752e24ac3ef71353b05e49c45daa150b7be44bbf
Kavinda <kbasnayake2000@gmail.com>
2025-10-22 02:21:05 +0530

    Array Changes

--- src/Demo.java
+++ src/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
     public static void main ( String [] args ){
         System.out.println ("Im Lakshan this is my First java Code");
 
-        int marks []= { 23, 67,34,78,90 };
+        int marks []= { 23, 67,34,78,90,45,56,77,67 };
         System.out.println(Arrays.toString(marks));
+
     }
 }
